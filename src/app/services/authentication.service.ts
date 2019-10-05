de45e4dec723b64a3581c3a24d9038207d8fbaf6
brianbad <brianbad@buffalo.edu>
2019-10-05 08:20:49 -0400

    Added env varibales for API paths

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  API_BASE_URL: string = "http://10.0.0.210:3000";
+  API_BASE_URL: string = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
